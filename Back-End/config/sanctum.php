699a1e73f379956ec25fc19d9cb32422015cd372
Amin332123 <mohamedaminerahmani6@gmail.com>
2026-06-02 11:54:42 +0100

    configuring the cors and also making google auth works

--- Back-End/config/sanctum.php
+++ Back-End/config/sanctum.php
@@ -20,7 +20,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,192.168.100.198,192.168.100.198:3000,192.168.100.198:8000',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
--- Front-end/next.config.ts
+++ Front-end/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.100.198"],
+  allowedDevOrigins: ["localhost", "127.0.0.1"],
 };
 
 export default nextConfig;
