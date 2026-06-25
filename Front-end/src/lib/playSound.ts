a1891c505d4777904a71a8fec574f7e5be36b9fe
tawdev <ahlanbekgroup@gmail.com>
2026-06-25 12:43:24 +0100

    i add a sounds

--- Front-end/src/lib/playSound.ts
+++ Front-end/src/lib/playSound.ts
@@ -2,25 +2,9 @@
 
 export function playConfirmationSound() {
   try {
-    const ctx = new (window.AudioContext || (window as any).webkitAudioContext)();
-
-    const playTone = (freq: number, start: number, duration: number) => {
-      const osc = ctx.createOscillator();
-      const gain = ctx.createGain();
-      osc.type = "sine";
-      osc.frequency.value = freq;
-      gain.gain.setValueAtTime(0.3, start);
-      gain.gain.exponentialRampToValueAtTime(0.01, start + duration);
-      osc.connect(gain);
-      gain.connect(ctx.destination);
-      osc.start(start);
-      osc.stop(start + duration);
-    };
-
-    const t = ctx.currentTime;
-    playTone(523.25, t, 0.15);
-    playTone(659.25, t + 0.12, 0.15);
-    playTone(783.99, t + 0.24, 0.3);
+    const audio = new Audio("/confirmation.mp3");
+    audio.volume = 0.5;
+    audio.play();
   } catch {
     // Audio not supported, silently ignore
   }
